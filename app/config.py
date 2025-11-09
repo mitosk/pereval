f521b64a2a0243147ccf482d495858b6f39b5580
mitosk <mltosk777@gmail.com>
2025-11-09 14:55:09 +0300

    Обновляем доступ к базе

--- app/config.py
+++ app/config.py
@@ -3,5 +3,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# Для подключения к PostgreSQL через SQLAlchemy
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@localhost:5432/pereval_db")
+# Получаем данные подключения из переменных окружения
+DB_HOST = os.getenv("FSTR_DB_HOST", "localhost")
+DB_PORT = os.getenv("FSTR_DB_PORT", "5432")
+DB_USER = os.getenv("FSTR_DB_LOGIN", "postgres")
+DB_PASS = os.getenv("FSTR_DB_PASS", "postgres")
+DB_NAME = os.getenv("FSTR_DB_NAME", "pereval_db")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
